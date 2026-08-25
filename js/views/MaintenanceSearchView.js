4aa0eb96e73eb3e25ec95c301757f8656b4365a7
Mohamed202620 <mom379339@gmail.com>
2026-08-26 01:27:34 +0300

    Add files via upload

--- js/views/MaintenanceSearchView.js
+++ js/views/MaintenanceSearchView.js
@@ -8,7 +8,7 @@
 // ============================================================
 
 export const MaintenanceSearchView = () => `
-<div class="p-4 max-w-md mx-auto pb-24 space-y-4 text-white">
+<div class="app-page p-4 max-w-md mx-auto pb-24 space-y-4 text-white">
 
   <!-- زر الرجوع -->
   <button
