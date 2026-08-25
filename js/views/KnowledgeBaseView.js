d7d1ec67e8c1f5c6be4c1b981c10b40ae8f5ff5f
Mohamed202620 <mom379339@gmail.com>
2026-08-26 01:25:17 +0300

    Add files via upload

--- js/views/KnowledgeBaseView.js
+++ js/views/KnowledgeBaseView.js
@@ -1,5 +1,5 @@
 export const KnowledgeBaseView = () => `
-<div class="p-4 max-w-md mx-auto pb-24 space-y-4 text-white">
+<div class="app-page p-4 max-w-md mx-auto pb-24 space-y-4 text-white">
 
   <!-- زر الرجوع -->
   <button
