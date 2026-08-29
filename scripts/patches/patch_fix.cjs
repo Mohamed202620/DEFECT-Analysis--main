eec9b305fab064f2b288e05e16d1ecb12400d82c
Mohamed202620 <mom379339@gmail.com>
2026-08-30 01:47:36 +0300

    Delete scripts/patches directory

--- scripts/patches/patch_fix.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = 'js/services/exportUtility.js';
-let content = fs.readFileSync(path, 'utf8');
-
-content = content.replace(/\\\`/g, '`');
-content = content.replace(/\\\$\{/g, '${');
-
-fs.writeFileSync(path, content, 'utf8');
