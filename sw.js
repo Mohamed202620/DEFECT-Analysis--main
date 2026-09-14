86b822ce326b18c658a132e40b220c6893f4a172
Mohamed202620 <mom379339@gmail.com>
2026-09-14 23:06:46 +0000

    Refresh service worker cache version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // تحديث رقم الإصدار مهم جداً عندما تقوم بتعديل أي ملف ليقوم المتصفح بتحديث الكاش
-const CACHE_NAME = 'maint-system-v5.8';
+const CACHE_NAME = 'maint-system-v5.9';
 
 // نكتفي بالملفات الأساسية المضمونة لتجنب فشل التثبيت
 const CORE_ASSETS = [
