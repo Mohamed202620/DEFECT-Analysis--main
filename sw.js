2445e44f51d109990e2a098a89c5fb2bca41580a
Mohamed202620 <mom379339@gmail.com>
2026-09-19 08:50:52 +0300

    fix(mobile): optimize mobile view and prevent zooming
    Restrict viewport scaling to native app behavior and update service worker cache version to v6.0.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // تحديث رقم الإصدار مهم جداً عندما تقوم بتعديل أي ملف ليقوم المتصفح بتحديث الكاش
-const CACHE_NAME = 'maint-system-v5.9';
+const CACHE_NAME = 'maint-system-v6.0';
 
 // نكتفي بالملفات الأساسية المضمونة لتجنب فشل التثبيت
 const CORE_ASSETS = [
